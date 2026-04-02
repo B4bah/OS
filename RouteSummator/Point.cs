bc3982fda03710e5690c050a0f54067764ba1f2c
B4bah <osipovmisha0909@yandex.ru>
2026-04-02 18:04:22 +0300

    Started working on a homework №3

---
+++ RouteSummator/Point.cs
@@ -0,0 +1,19 @@
+﻿namespace RouteSummator
+{
+    public struct Point
+    {
+        public double X;
+        public double Y;
+
+        public Point(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public override string ToString()
+        {
+            return $"({X.ToString()}, {Y.ToString()})";
+        }
+    }
+}
---
+++ RouteSummator/Route.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RouteSummator
+{
+    public class Route : IRoute
+    {
+        private List<Point> _points;
+
+        public Route()
+        {
+            _points = new List<Point>();
+        }
+
+        public void Fill()
+        {
+
+        }
+    }
+}
