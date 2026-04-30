005ae139109e02ceee887df2d870f6e5702342b1
B4bah <osipovmisha09092006@gmail.com>
2026-05-01 02:00:42 +0300

    Finishing task from class

---
+++ GenPrinter-2/SquarerInt.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenPrinter_2
+{
+    public class SquarerInt : ActiveObject
+    {
+        private BlockingBuffer<int> _in;
+        private BlockingBuffer<int> _out;
+
+        public BlockingBuffer<int> Out { get { return _out; }  }
+
+        public SquarerInt(BlockingBuffer<int> inBuff, int bufferSize)
+        {
+            _in = inBuff;
+            _out = new BlockingBuffer<int>(bufferSize);
+        }
+
+        protected override void Run()
+        {
+            while(!IsStopped)
+            {
+                int value = _in.Pop();
+
+                value = value * value;
+
+                _out.Add(value);
+            }
+        }
+    }
+}
